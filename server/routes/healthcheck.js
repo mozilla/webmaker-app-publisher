13f16cbe485903a41f4ba31633ab54d78f9c0a0a
Kate Hudson <k88hudson@gmail.com>
2014-10-03 15:40:15 -0400

    syntax

--- server/routes/healthcheck.js
+++ server/routes/healthcheck.js
@@ -1,7 +1,7 @@
 module.exports = function (req, res, next) {
     var habitat = require('habitat');
     console.log({
-        ALLOWED_DOMAINS: habitat.get('ALLOWED_DOMAINS');
+        ALLOWED_DOMAINS: habitat.get('ALLOWED_DOMAINS')
     });
     res.send({
         version: require('../../package.json').version,
